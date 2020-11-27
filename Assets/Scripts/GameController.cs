24fa041419b0d80c09c608858fae27b72bbc139c
Rishi Raja <rishiraja36@gmail.com>
2020-11-27 11:18:32 +0530

    Full working Tower of Hanoi game with automatic solution.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject[] disks;//Discs Refrences
+    // wavepoints on top of tower for snapping and moving purpose
+    public Vector3 waypointA;
+    public Vector3 waypointB;
+    public Vector3 waypointC;
+
+    public int numberOfDiscs = 6,moves= 0, winState=0;
+    public float speed = 1,hieghtFactor=1.2f, timer;
+    public bool isAnimating;
+
+    public Text timerTxt, movesTxt,alertTxt,totalTimeTxt,totalMovesTxt;
+    public GameObject pausePanel, alertPanel, gameCompletePanel;
+
+    //List of sequence of discs and respective target position
+    List<GameObject> diskSeq = new List<GameObject>();
+    List<Vector3> posASeq = new List<Vector3>();
+    List<Vector3> posBSeq = new List<Vector3>();
+    int i = -1;
+
+    // Use this for initialization
+    void Start ()
+    {
+        // assigning the referenes
+        waypointA = GameObject.Find("wayPoint A").transform.position;
+        waypointB = GameObject.Find("wayPoint B").transform.position;
+        waypointC = GameObject.Find("wayPoint C").transform.position;
+        ResetGame();
+    }
+
+    // Update is called once per frame
+    
+    void Update ()
+    {
+
+        if(!isAnimating && winState == 0)
+        {
+            timer += Time.deltaTime;
+           
+            timerTxt.text = floatToTime(timer);
+        }
+       
+        movesTxt.text ="Moves :"+ moves.ToString();
+    }
+
+    // Resets the game
+    public void ResetGame()
+    {
+        for (int i = 0; i < disks.Length; i++)
+        {
+            if (i < numberOfDiscs)
+            {
+                disks[i].SetActive(true);
+                Vector3 pos = waypointA;
+                pos.y = (disks[i].transform.localScale.y * hieghtFactor) * (numberOfDiscs - i - 1);
+                disks[i].transform.position = pos;
+            }
+            else
+            {
+                disks[i].SetActive(false);
+
+            }
+        }
+
+        moves = 0;
+        timer = 0;
+        winState = 0;
+
+    }
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        ResetGame();
+        gameCompletePanel.SetActive(false);
+        pausePanel.SetActive(false);
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        alertPanel.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        alertPanel.SetActive(false);
+    }
+
+    public void ShowAlert(string message,float time)
+    {
+        StartCoroutine(IShowAlert(message, time));
+    }
+
+    public void GameComplete()
+    {
+        gameCompletePanel.SetActive(true);
+        totalMovesTxt.text = "Total Moves : "+moves.ToString();
+        totalTimeTxt.text = "Total Time : " + floatToTime(timer);
+    }
+
+    string floatToTime(float timer)
+    {
+        string minutes = Mathf.Floor(timer / 60).ToString("00");
+        string seconds = (timer % 60).ToString("00");
+        return string.Format("{0}m:{1}s", minutes, seconds);
+    }
+
+    IEnumerator IShowAlert(string message,float closeTime)
+    {
+        alertPanel.SetActive(true);
+        alertTxt.text = message;
+        yield return new WaitForSeconds(closeTime);
+        alertPanel.SetActive(false);
+    }
+
+    //region of automatic solution
+    #region
+
+    //Show animating sequences for tower of hanoi solution
+    public void ShowSolution()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        ResetGame();
+        hanoi(numberOfDiscs, waypointA, waypointC, waypointB);
+        Debug.Log("IN ARRAYS===>");
+        for (int i = 0; i < Mathf.Pow(2, numberOfDiscs) - 1; i++)
+        {
+            Debug.Log("Move disk " + diskSeq[i].name + " from " + posASeq[i] + " to " + posBSeq[i]);
+        }
+        isAnimating = true;
+        StartCoroutine(moveAll());
+    }
+
+    // Moving discs as per sequence to thier target location as per the list
+    IEnumerator moveAll()
+    {
+        for (int i = 0; i < Mathf.Pow(2, numberOfDiscs) - 1; i++)
+        {
+            yield return StartCoroutine(move(diskSeq[i], posASeq[i], posBSeq[i]));
+            if (i == Mathf.Pow(2, numberOfDiscs) - 3)
+                winState++;
+        }
+        isAnimating = false;
+        GameComplete();
+    }
+
+    // A recurrsive function to find the solution for the given discs
+    void hanoi(int n, Vector3 from, Vector3 to, Vector3 aux)
+    {
+        if (n == 1)
+        {
+            addToSequence(disks[0], from, to);
+            return;
+        }
+        hanoi(n - 1, from, aux, to);
+        addToSequence(disks[n - 1], from, to);
+        hanoi(n - 1, aux, to, from);
+    }
+
+    // Saving the sequences in the lists which disc should move to which location
+    void addToSequence(GameObject disk, Vector3 from, Vector3 to)
+    {
+        i++;
+        diskSeq.Add(disk);
+        posASeq.Add(from);
+        posBSeq.Add(to);
+
+    }
+
+    // moving the disc to thier targeted location using itween for tweening
+    public IEnumerator move(GameObject disk, Vector3 posA, Vector3 posB)
+    {
+        iTween.MoveTo(disk, posA, 2 / speed);
+        yield return new WaitForSeconds(2 / speed);
+        iTween.MoveTo(disk, posB, 1 / speed);
+        yield return new WaitForSeconds(1 / speed);
+
+        RaycastHit hit;
+        if (Physics.Raycast(posB + Vector3.down * 2, Vector3.down, out hit, 6))
+        {
+            iTween.MoveTo(disk, hit.point + Vector3.up * .3f, 2 / speed);
+            yield return new WaitForSeconds(2 / speed);
+        }
+    }
+    #endregion
+
+
+
+}
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,254 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    public GameObject target;
+    private bool isMouseDrag;
+    public GameController gameController;
+    private Vector3 screenPosition, offset,lastPosition;
+    public float snapDistance;
+    public LayerMask m_Mask = -1;
+    private float hitClamp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0) && gameController.winState ==0  && gameController.isAnimating == false)
+        {
+            RaycastHit hitInfo;
+            target = ReturnClickedObject(out hitInfo);
+            if (target != null)
+            {
+                isMouseDrag = true;
+                lastPosition = target.transform.position;
+                //Convert world position to screen position.
+                screenPosition = Camera.main.WorldToScreenPoint(target.transform.position);
+                offset = target.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z));
+            }
+            hitClamp = 0;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isMouseDrag = false;
+            hitClamp = 0;
+            if(target!=null)
+            CheckDiscPosition();
+            target = null;
+        }
+
+        if (isMouseDrag)
+        {
+            //track mouse position.
+            Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z);
+
+            //convert screen position to world position with offset changes.
+            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenSpace) + offset;
+            currentPosition.z = 0;
+ 
+            RaycastHit hit;
+            
+            if (Physics.Raycast(target.transform.position, Vector3.down, out hit, 5, m_Mask.value, QueryTriggerInteraction.Collide))
+            {
+                hitClamp = hit.transform.position.y + target.transform.localScale.y * gameController.hieghtFactor;
+            }
+            else
+            {
+                hitClamp = 0;
+            }
+
+            currentPosition.y = Mathf.Clamp(currentPosition.y, hitClamp, gameController.waypointA.y);
+
+            if (currentPosition.y < gameController.waypointA.y)
+            {
+                if (target.transform.position.x < gameController.waypointA.x + snapDistance)
+                {
+                    currentPosition.x = gameController.waypointA.x;
+                }
+                else if (target.transform.position.x > gameController.waypointC.x - snapDistance)
+                {
+                    currentPosition.x = gameController.waypointC.x;
+                }
+                else if (target.transform.position.x < gameController.waypointB.x + snapDistance && target.transform.position.x > gameController.waypointB.x - snapDistance)
+                {
+                    currentPosition.x = gameController.waypointB.x;
+                }
+               /* else if ((target.transform.position.x > gameController.waypointA.x + movementOffset && target.transform.position.x < gameController.waypointB.x - movementOffset)
+                    || (target.transform.position.x > gameController.waypointB.x + movementOffset && target.transform.position.x < gameController.waypointC.x - movementOffset))
+                {
+                    currentPosition.y = gameController.waypointA.y;
+                }
+                else
+                {
+                    currentPosition.x = gameController.waypointB.x;
+                }*/
+
+               
+            }
+            else
+            {
+               
+                currentPosition.x = Mathf.Clamp(currentPosition.x, gameController.waypointA.x, gameController.waypointC.x);
+            }
+            
+            
+            target.transform.position = currentPosition;
+        }
+
+    }
+
+    public void CheckDiscPosition()
+    {
+
+        RaycastHit hit;
+        Vector3 snapPos = new Vector3();
+
+        if (target.transform.position.x < gameController.waypointA.x + snapDistance)
+        {
+            snapPos = gameController.waypointA;
+            snapPos.y = 0;
+
+            if (Physics.Raycast(target.transform.position, Vector3.down, out hit, 5, m_Mask.value, QueryTriggerInteraction.Collide))
+            {
+                Debug.Log(hit.transform.name + " " + hit.transform.position.y + " " + hitClamp+ " "+target.transform.name);
+
+                    hitClamp = hit.transform.position.y + target.transform.localScale.y * gameController.hieghtFactor;
+                   
+                    if (hit.transform.localScale.x < target.transform.localScale.x)
+                    {
+                        Debug.Log("Can't place it here" + lastPosition);
+                        snapPos = lastPosition;
+                    }
+                    else
+                    {
+                        //  snapPos = hit.transform.position;
+                        snapPos.y = hitClamp;
+                    }
+                
+                //}
+            }
+
+            Debug.Log("Move to A" + snapPos + " " + gameController.waypointA);
+        }
+        else if (target.transform.position.x > gameController.waypointC.x - snapDistance)
+        {
+            snapPos = gameController.waypointC;
+            snapPos.y = 0;
+
+            if (Physics.Raycast(target.transform.position, Vector3.down, out hit, 5, m_Mask.value, QueryTriggerInteraction.Collide))
+            {
+                Debug.Log(hit.transform.name + " " + hit.transform.position.y + " " + hitClamp + " " + target.transform.name);
+
+                
+                    hitClamp = hit.transform.position.y + target.transform.localScale.y * gameController.hieghtFactor;
+                    
+                    if (hit.transform.localScale.x < target.transform.localScale.x)
+                    {
+                        Debug.Log("Can't place it here" + lastPosition);
+                        snapPos = lastPosition;
+                    }
+                    else
+                    {
+                        snapPos.y = hitClamp;
+                    }
+                
+            }
+            Debug.Log("Move to C" + snapPos + " " + gameController.waypointC);
+        }
+        else if (target.transform.position.x < gameController.waypointB.x + snapDistance && target.transform.position.x > gameController.waypointB.x - snapDistance)
+        {
+            snapPos = gameController.waypointB;
+            snapPos.y = 0;
+            if (Physics.Raycast(target.transform.position, Vector3.down, out hit, 5, m_Mask.value, QueryTriggerInteraction.Collide))
+            {
+                Debug.Log(hit.transform.name + " " + hit.transform.position.y + " " + hitClamp + " " + target.transform.name);
+
+                    hitClamp = hit.transform.position.y + target.transform.localScale.y * gameController.hieghtFactor;
+
+                    Debug.Log(hit.transform.name + " " + hit.transform.position.y + " " + hitClamp);
+
+                    if (hit.transform.localScale.x < target.transform.localScale.x)
+                    {
+                        Debug.Log("Can't place it here" + lastPosition);
+                        gameController.ShowAlert("Can't place bigger disc on top of smaller disc.", 2);
+                        snapPos = lastPosition;
+                    }
+                    else
+                    {
+                        snapPos.y = hitClamp;
+                    }
+                
+            }
+            Debug.Log("Move to B" + snapPos + " " + gameController.waypointB);
+        }
+        else
+        {
+            Debug.Log("Can't snap to any tower move back" + lastPosition);
+            gameController.ShowAlert("Please drop the disc on top of tower.",2);
+            snapPos = lastPosition;
+        }
+  
+        MoveTarget(snapPos);
+    }
+
+    public void MoveTarget(Vector3 position)
+    {
+        gameController.moves++;
+        float distance = Vector3.Distance(target.transform.position, position);
+       
+
+       // iTween.MoveTo(target, position, distance / 10);
+        iTween.MoveTo(target, iTween.Hash(
+            "position", position,
+            "time", distance / 10,
+            "oncomplete", "CheckGameComplete",
+            "oncompletetarget", gameObject
+            ));
+    }
+
+    public void CheckGameComplete()
+    {
+        Debug.Log("Check Complete");
+
+        for(int i=0;i<gameController.numberOfDiscs;i++)
+        {
+            if(!Mathf.Approximately(gameController.disks[i].transform.position.x,gameController.waypointC.x))
+            {
+                return;
+            }
+            if(i==gameController.numberOfDiscs-1)
+            {
+                gameController.winState++;
+                Debug.Log("Game Complete");
+                gameController.GameComplete();
+            }
+        }
+    }
+
+    // Return the selected object 
+    GameObject ReturnClickedObject(out RaycastHit hit)
+    {
+        GameObject target = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray.origin,ray.direction* 10, out hit,10, m_Mask.value, QueryTriggerInteraction.Ignore))
+        {
+            target = hit.collider.gameObject;
+
+            if (Physics.Raycast(target.transform.position, Vector3.up, out hit, 1, m_Mask.value, QueryTriggerInteraction.Ignore))
+            {
+                target = null;
+            }
+            
+        }
+        return target;
+    }
+}
