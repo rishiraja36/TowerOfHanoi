84e837df37acd67024a38a4cac1cc699a7848082
Rishi Raja <rishiraja36@gmail.com>
2020-11-27 11:46:30 +0530

    Added comments in touch controller.

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class TouchController : MonoBehaviour
 {
-    public GameObject target;
-    private bool isMouseDrag;
+    public GameObject target;// saves the selected discs refernce
     public GameController gameController;
-    private Vector3 screenPosition, offset,lastPosition;
-    public float snapDistance;
-    public LayerMask m_Mask = -1;
-    private float hitClamp;
+    public float snapDistance; // distance w.r.t tower after dropping the disc
+    public LayerMask m_Mask = -1; // to cast ray only on discs
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool isMouseDrag;
+    private float hitClamp; //y position after hitting any disc while moving or snapping
+    private Vector3 screenPosition, offset, lastPosition;
 
     void Update()
     {
@@ -24,7 +19,9 @@ public class TouchController : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && gameController.winState ==0  && gameController.isAnimating == false)
         {
             RaycastHit hitInfo;
+
             target = ReturnClickedObject(out hitInfo);
+
             if (target != null)
             {
                 isMouseDrag = true;
@@ -40,8 +37,11 @@ public class TouchController : MonoBehaviour
         {
             isMouseDrag = false;
             hitClamp = 0;
+
+            //don't check for position if no disc was selected
             if(target!=null)
-            CheckDiscPosition();
+                CheckDiscPosition();
+
             target = null;
         }
 
@@ -55,47 +55,42 @@ public class TouchController : MonoBehaviour
             currentPosition.z = 0;
  
             RaycastHit hit;
-            
+
+            //Stop moving the disc in y if if stacks on another disc
             if (Physics.Raycast(target.transform.position, Vector3.down, out hit, 5, m_Mask.value, QueryTriggerInteraction.Collide))
             {
                 hitClamp = hit.transform.position.y + target.transform.localScale.y * gameController.hieghtFactor;
             }
             else
             {
+                // no disc found inside the current 
                 hitClamp = 0;
             }
 
             currentPosition.y = Mathf.Clamp(currentPosition.y, hitClamp, gameController.waypointA.y);
 
+            //Check if disc is inside the tower or above tower
             if (currentPosition.y < gameController.waypointA.y)
             {
+                //if disc is inside the tower 'A' then snap to it
                 if (target.transform.position.x < gameController.waypointA.x + snapDistance)
                 {
                     currentPosition.x = gameController.waypointA.x;
                 }
+                //if disc is inside the tower 'B' then snap to it
                 else if (target.transform.position.x > gameController.waypointC.x - snapDistance)
                 {
                     currentPosition.x = gameController.waypointC.x;
                 }
+                //if disc is inside the tower 'C' then snap to it
                 else if (target.transform.position.x < gameController.waypointB.x + snapDistance && target.transform.position.x > gameController.waypointB.x - snapDistance)
                 {
                     currentPosition.x = gameController.waypointB.x;
                 }
-               /* else if ((target.transform.position.x > gameController.waypointA.x + movementOffset && target.transform.position.x < gameController.waypointB.x - movementOffset)
-                    || (target.transform.position.x > gameController.waypointB.x + movementOffset && target.transform.position.x < gameController.waypointC.x - movementOffset))
-                {
-                    currentPosition.y = gameController.waypointA.y;
-                }
-                else
-                {
-                    currentPosition.x = gameController.waypointB.x;
-                }*/
-
-               
             }
             else
             {
-               
+               //Disc is above the tower so fix the y position and clamp x between tower 'A' and 'C'
                 currentPosition.x = Mathf.Clamp(currentPosition.x, gameController.waypointA.x, gameController.waypointC.x);
             }
             
@@ -105,107 +100,74 @@ public class TouchController : MonoBehaviour
 
     }
 
+    // Check disc position after release 
     public void CheckDiscPosition()
     {
 
         RaycastHit hit;
         Vector3 snapPos = new Vector3();
 
+        //if near to wavepoint 'A' within snap distance then snap to 'A'
         if (target.transform.position.x < gameController.waypointA.x + snapDistance)
         {
             snapPos = gameController.waypointA;
             snapPos.y = 0;
 
-            if (Physics.Raycast(target.transform.position, Vector3.down, out hit, 5, m_Mask.value, QueryTriggerInteraction.Collide))
-            {
-                Debug.Log(hit.transform.name + " " + hit.transform.position.y + " " + hitClamp+ " "+target.transform.name);
-
-                    hitClamp = hit.transform.position.y + target.transform.localScale.y * gameController.hieghtFactor;
-                   
-                    if (hit.transform.localScale.x < target.transform.localScale.x)
-                    {
-                        Debug.Log("Can't place it here" + lastPosition);
-                        snapPos = lastPosition;
-                    }
-                    else
-                    {
-                        //  snapPos = hit.transform.position;
-                        snapPos.y = hitClamp;
-                    }
-                
-                //}
-            }
-
-            Debug.Log("Move to A" + snapPos + " " + gameController.waypointA);
         }
-        else if (target.transform.position.x > gameController.waypointC.x - snapDistance)
+        //if near to wavepoint 'B' within snap distance then snap to 'B'
+        else if (target.transform.position.x > gameController.waypointC.x - snapDistance) 
         {
             snapPos = gameController.waypointC;
             snapPos.y = 0;
 
-            if (Physics.Raycast(target.transform.position, Vector3.down, out hit, 5, m_Mask.value, QueryTriggerInteraction.Collide))
-            {
-                Debug.Log(hit.transform.name + " " + hit.transform.position.y + " " + hitClamp + " " + target.transform.name);
-
-                
-                    hitClamp = hit.transform.position.y + target.transform.localScale.y * gameController.hieghtFactor;
-                    
-                    if (hit.transform.localScale.x < target.transform.localScale.x)
-                    {
-                        Debug.Log("Can't place it here" + lastPosition);
-                        snapPos = lastPosition;
-                    }
-                    else
-                    {
-                        snapPos.y = hitClamp;
-                    }
-                
-            }
-            Debug.Log("Move to C" + snapPos + " " + gameController.waypointC);
         }
+        //if near to wavepoint 'C' within snap distance then snap to 'C'
         else if (target.transform.position.x < gameController.waypointB.x + snapDistance && target.transform.position.x > gameController.waypointB.x - snapDistance)
         {
             snapPos = gameController.waypointB;
             snapPos.y = 0;
-            if (Physics.Raycast(target.transform.position, Vector3.down, out hit, 5, m_Mask.value, QueryTriggerInteraction.Collide))
-            {
-                Debug.Log(hit.transform.name + " " + hit.transform.position.y + " " + hitClamp + " " + target.transform.name);
 
-                    hitClamp = hit.transform.position.y + target.transform.localScale.y * gameController.hieghtFactor;
-
-                    Debug.Log(hit.transform.name + " " + hit.transform.position.y + " " + hitClamp);
-
-                    if (hit.transform.localScale.x < target.transform.localScale.x)
-                    {
-                        Debug.Log("Can't place it here" + lastPosition);
-                        gameController.ShowAlert("Can't place bigger disc on top of smaller disc.", 2);
-                        snapPos = lastPosition;
-                    }
-                    else
-                    {
-                        snapPos.y = hitClamp;
-                    }
-                
-            }
-            Debug.Log("Move to B" + snapPos + " " + gameController.waypointB);
         }
         else
         {
-            Debug.Log("Can't snap to any tower move back" + lastPosition);
+            //Disc dropped out of snap distance between towers move back to last position
             gameController.ShowAlert("Please drop the disc on top of tower.",2);
             snapPos = lastPosition;
         }
-  
+
+        //Check y position of selected disc after dropping and snapping
+        //if any disc is already in the tower then stack on top of that
+        if (Physics.Raycast(target.transform.position, Vector3.down, out hit, 5, m_Mask.value, QueryTriggerInteraction.Collide))
+        {
+            hitClamp = hit.transform.position.y + target.transform.localScale.y * gameController.hieghtFactor;
+
+            if (hit.transform.localScale.x < target.transform.localScale.x)
+            {
+                Debug.Log("Can't place it here" + lastPosition);
+                gameController.ShowAlert("Can't place bigger disc on top of smaller disc.", 2);
+                snapPos = lastPosition;
+            }
+            else
+            {
+                snapPos.y = hitClamp;
+            }
+
+        }
+
+        // Move to final snap position after snap and stack check
         MoveTarget(snapPos);
     }
+
+
 
     public void MoveTarget(Vector3 position)
     {
         gameController.moves++;
+        //speed on animation according to the current disc postion and snapped tower position
         float distance = Vector3.Distance(target.transform.position, position);
        
 
-       // iTween.MoveTo(target, position, distance / 10);
+       //Tween to target location and check for game complete condition after moving
         iTween.MoveTo(target, iTween.Hash(
             "position", position,
             "time", distance / 10,
@@ -214,16 +176,20 @@ public class TouchController : MonoBehaviour
             ));
     }
 
+    //Checks whether game is completed or not
     public void CheckGameComplete()
     {
         Debug.Log("Check Complete");
 
         for(int i=0;i<gameController.numberOfDiscs;i++)
         {
+            //Checks if any disc is not in towerC then return
             if(!Mathf.Approximately(gameController.disks[i].transform.position.x,gameController.waypointC.x))
             {
                 return;
             }
+
+            //Checks if it is last disc then show complete
             if(i==gameController.numberOfDiscs-1)
             {
                 gameController.winState++;
@@ -233,7 +199,7 @@ public class TouchController : MonoBehaviour
         }
     }
 
-    // Return the selected object 
+    // Return the selected disc 
     GameObject ReturnClickedObject(out RaycastHit hit)
     {
         GameObject target = null;
